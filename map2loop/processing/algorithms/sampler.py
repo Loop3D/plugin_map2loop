f67be330d9a63cb7543ab7e9b4e58e2e252e3cfa
rabii-chaarani <rabii.chaarani@monash.edu>
2025-08-25 14:27:31 +0930

    feature: add support SamplerSpacing

--- map2loop/processing/algorithms/sampler.py
+++ map2loop/processing/algorithms/sampler.py
@@ -137,11 +137,16 @@ class SamplerAlgorithm(QgsProcessingAlgorithm):
         spatial_data = qgsLayerToGeoDataFrame(spatial_data)
         
         if sampler_type == "SamplerDecimator":
-        feedback.pushInfo("Sampling...")
-        sampler = SamplerDecimator(decimation=decimation, dtm_data=dtm, geology_data=geology, feedback=feedback)
-        samples = sampler.sample(spatial_data)
-    
-        samples = qgs
+            feedback.pushInfo("Sampling...")
+            sampler = SamplerDecimator(decimation=decimation, dtm_data=dtm, geology_data=geology, feedback=feedback)
+            samples = sampler.sample(spatial_data)
+        if sampler_type == "SamplerSpacing":
+            feedback.pushInfo("Sampling...")
+            sampler = SamplerSpacing(spacing=spacing, dtm_data=dtm, geology_data=geology, feedback=feedback)
+            samples = sampler.sample(spatial_data)
+
+        #TODO: convert sample to qgis layer
+        # samples = qgs
         return {self.OUTPUT: samples}
 
     def createInstance(self) -> QgsProcessingAlgorithm:
