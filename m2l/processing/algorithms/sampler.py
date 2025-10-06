2cd4c536f392f8bb6cb988e465ff53f39fa616bc
Noelle Cheng <noelle.cheng@monash.edu>
2025-10-06 14:06:04 +0800

    feat dynamic field handling in Sampler

--- m2l/processing/algorithms/sampler.py
+++ m2l/processing/algorithms/sampler.py
@@ -176,11 +176,19 @@ class SamplerAlgorithm(QgsProcessingAlgorithm):
             samples = sampler.sample(spatial_data_gdf)
         
         fields = QgsFields()
-        fields.append(QgsField("ID", QMetaType.Type.QString))
-        fields.append(QgsField("X", QMetaType.Type.Float))
-        fields.append(QgsField("Y", QMetaType.Type.Float))
-        fields.append(QgsField("Z", QMetaType.Type.Float))
-        fields.append(QgsField("featureId", QMetaType.Type.QString))
+        if samples is not None and not samples.empty:
+            for column_name in samples.columns:
+                dtype = samples[column_name].dtype
+                dtype_str = str(dtype)
+            
+                if dtype_str in ['float16', 'float32', 'float64']:
+                    field_type = QMetaType.Type.Double
+                elif dtype_str in ['int8', 'int16', 'int32', 'int64']:
+                    field_type = QMetaType.Type.Int
+                else:
+                    field_type = QMetaType.Type.QString 
+                
+                fields.append(QgsField(column_name, field_type))
 
         crs = None
         if spatial_data_gdf is not None and spatial_data_gdf.crs is not None:
@@ -207,13 +215,21 @@ class SamplerAlgorithm(QgsProcessingAlgorithm):
                     #spacing has no z values
                     feature.setGeometry(QgsGeometry.fromPointXY(QgsPointXY(row['X'], row['Y'])))
                 
-                feature.setAttributes([
-                    str(row.get('ID', '')),
-                    float(row.get('X', 0)),
-                    float(row.get('Y', 0)),
-                    float(row.get('Z', 0)) if pd.notna(row.get('Z')) else 0.0,
-                    str(row.get('featureId', ''))
-                ])
+                attributes = []
+                for column_name in samples.columns:
+                    value = row.get(column_name)
+                    dtype = samples[column_name].dtype
+                    
+                    if pd.isna(value):
+                        attributes.append(None)
+                    elif dtype in ['float16', 'float32', 'float64']:
+                        attributes.append(float(value))
+                    elif dtype in ['int8', 'int16', 'int32', 'int64']:
+                        attributes.append(int(value))
+                    else:
+                        attributes.append(str(value))
+                
+                feature.setAttributes(attributes)
                 
                 sink.addFeature(feature)
 
